d9bef54cdac16c8ecf06f4a50e6d555c55e084a3
Oleh Onyshchak <oleh.onyshchak@gmail.com>
2019-10-24 20:13:19 +0300

    Fixed errors with articles/images downloading. Downloaded entire dataset with new script

--- article_reader/reader.py
+++ article_reader/reader.py
@@ -41,18 +41,51 @@ def get_url(img_name, size=600):
         
     return url
 
-def query(filename, out_dir='../data/', debug_info=True, limit=None):   
+def img_download(img_links, page_dir, tc, uc):
+    img_dir = get_path(page_dir/"img")
+    for img in img_links:
+        img_name = img.title(as_filename=True, with_ns=False)            
+        img_name_valid = hashlib.md5(img_name.encode('utf-8')).hexdigest()
+            
+        img_path = img_dir / (img_name_valid + ".jpg")
+        if img_path.exists(): continue
+            
+        img_path_orig = Path(str(img_path) + "_" + img_name + ".ORIGINAL")
+        if img_path_orig.exists(): continue
+            
+        tc += 1
+        try:
+            urlretrieve(get_url(img_name), img_path)
+        except:
+            uc += 1
+            img.download(filename=img_path_orig, chunk_size=8*1024)
+            
+    return (tc, uc)
+
+def query(filename, out_dir='../data/', debug_info=True, offset=0, limit=None):   
     print('Downloading...')
     site = pywikibot.Site()    
     pages = list(pagegenerators.TextfilePageGenerator(filename=filename, site=site))
+    if not limit:
+        limit = len(pages) + 1
     
     prev_percent = -1
-    unavailable_count = 0
-    total_count = 0
-    for i, p in enumerate(pages):
-        if i >= limit: break
+    tc = 0
+    uc = 0
+    for i in range(offset, min(offset + limit, len(pages))):
+        p = pages[i]
+        if p.pageid == 0:
+            print("ERROR: Cannot fetch the page " + p.title())
+            continue
             
-        page_dir = get_path(out_dir + p.title(as_filename=True))
+        if debug_info:
+            percent = int(i / len(pages) * 100)
+            if prev_percent != percent:
+                prev_percent = percent
+                print('{}% completed'.format(percent))
+            
+        
+        page_dir = get_path(out_dir + p.title(as_filename=True).rstrip('.'))
         text_path = page_dir / 'text.json'
         if not text_path.exists():
             page_json = json.dumps({
@@ -63,33 +96,11 @@ def query(filename, out_dir='../data/', debug_info=True, limit=None):
             })
             
             _dump(text_path, page_json)
-        
-        # TODO: fix the problem with pageid == 0
-        if p.pageid == 0:
-            print("ERROR: Cannot fetch the page " + p.title())
-            continue
-            
-        if debug_info:
-            count = min(limit, len(pages)) if limit else len(pages)
-            percent = int(i / count * 100)
-            if prev_percent != percent:
-                prev_percent = percent
-                print('{}% completed'.format(percent))
             
         # downloading page images
-        img_links = list(p.imagelinks())
-        img_dir = get_path(page_dir/"img")
-        for img in img_links:
-            img_name = img.title(as_filename=True, with_ns=False)
-            img_path = img_dir / (img_name + ".jpg")
-            if img_path.exists(): continue
+        try:
+            tc, uc = img_download(p.imagelinks(), page_dir, tc, uc) 
+        except:
+            print("ERROR: Cannot fetch all images from " + p.full_url())            
             
-            total_count += 1
-            try:
-                urlretrieve(get_url(img_name), img_path)
-            except:
-                unavailable_count += 1
-                img.download(filename=str(img_path) + ".ORIGINAL" , chunk_size=8*1024)
-            
-            
-    print('Downloaded {} images, where {} of them unavailable from commons'.format(total_count, unavailable_count))
+    print('Downloaded {} images, where {} of them unavailable from commons'.format(tc, uc))
